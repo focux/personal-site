2c29aa986bc05e56d51debd92123a853629d5c54
Leonardo E. Dominguez <focuux@gmail.com>
2020-05-25 01:08:49 -0400

    fix: render-blocking resources

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,7 @@ export default class MyDocument extends Document<{ ids: any[]; css: any }> {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
-          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          <link rel="preload" href="https://rsms.me/inter/inter.css" />
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -30,7 +30,7 @@ export default class MyDocument extends Document<{ ids: any[]; css: any }> {
             }}
           />
           <link
-            rel="stylesheet"
+            rel="preload"
             href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/night-owl.min.css"
           />
         </Head>
